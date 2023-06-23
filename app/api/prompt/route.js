2b17ce19ea133e0dfad697f389b56ad03b09fc93
pranshu199 <77734820+pranshu199@users.noreply.github.com>
2023-06-23 09:16:03 -0400

    improved performance of the application, made changes to route.js in api/prompt

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,15 +2,17 @@ import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
-    try {
-        await connectToDB()
+  try {
+    await connectToDB();
 
-        const prompts = await Prompt.find({}).populate("creator")
-        if (!prompts) return new Response("Prompt Not Found", { status: 404 });
-
-        return new Response(JSON.stringify(prompts), { status: 200 })
-
-    } catch (error) {
-        return new Response("Internal Server Error", { status: 500 });
+    const prompts = await Prompt.find({}).populate("creator");
+    if (!prompts || prompts.length === 0) {
+      return new Response("Prompt Not Found", { status: 404 });
     }
-}
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    console.error("Error retrieving prompts:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+};
