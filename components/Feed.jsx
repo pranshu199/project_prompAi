46b87613d5ff7df8a1aa6d3a54adcaddd08dbe92
pranshu199 <77734820+pranshu199@users.noreply.github.com>
2023-10-10 20:29:51 -0400

    fixed prompt renedring bug

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -35,7 +35,6 @@ const Feed = () => {
           throw new Error("Network response was not ok");
         }
         const data = await response.json();
-        console.log(data);
         setAllPosts(data);
       } catch (error) {
         console.error("Error fetching data:", error);
